59a568e2dc77363c868d1636ed87d2ee14170748
Sagar Verma <sagar15056@iiitd.ac.in>
2025-03-08 17:20:31 +0100

    train airplane myodm

--- myosuite/agents/brax/ppo_myodm_v0.py
+++ myosuite/agents/brax/ppo_myodm_v0.py
@@ -2,15 +2,21 @@ import os
 
 os.environ["JAX_CHECK_TRACER_LEAKS"] = "true"
 import functools
+import wandb 
+import mediapy as media
 
 from datetime import datetime
+import jax
 from jax import numpy as jp
 
 from brax import envs
+from brax.io import model
 from brax.training.agents.ppo import train as ppo
 
 from myosuite.envs.myo.myodm.myodm_v0_mjx import TrackEnv
 
+
+jax.config.update('jax_default_matmul_precision', 'highest')
 
 model_path = "/../assets/hand/myohand_object_mjx.xml"
 object_name = "airplane"
@@ -34,9 +40,9 @@ weighted_reward_keys = {
     "penalty": -2,
 }
 
-envs.register_environment("myodm_airplane_v0", TrackEnv)
+envs.register_environment("MyoHandAirplaneFly-v0", TrackEnv)
 
-env_name = "myodm_airplane_v0"
+env_name = "MyoHandAirplaneFly-v0"
 env = envs.get_environment(
     env_name, 
     model_path=model_path, 
@@ -58,28 +64,44 @@ def progress(num_steps, metrics):
     for key, value in metrics.items():
         print(f"  {key}: {value}")
 
+    wandb.log(step=num_steps, data=metrics)
 
-train_fn = functools.partial(
-    ppo.train,
-    num_timesteps=2_000_000,
-    num_evals=20,
-    reward_scaling=1,
-    episode_length=32,
-    normalize_observations=True,
-    action_repeat=1,
-    unroll_length=5,
-    num_minibatches=32,
-    num_updates_per_batch=4,
-    discounting=0.97,
-    learning_rate=3e-4,
-    entropy_cost=1e-2,
-    num_envs=2048,
-    batch_size=1024,
-    seed=1,
+
+# Define a configuration dictionary
+config = {
+    "num_timesteps": 100_000_000,
+    "num_evals": 1000,
+    "reward_scaling": 1,
+    "episode_length": 32,
+    "normalize_observations": True,
+    "action_repeat": 1,
+    "unroll_length": 5,
+    "num_minibatches": 32,
+    "num_updates_per_batch": 4,
+    "discounting": 0.97,
+    "learning_rate": 3e-4,
+    "entropy_cost": 1e-2,
+    "num_envs": 1024,
+    "batch_size": 512,
+    "seed": 1,
+}
+
+run = wandb.init(
+    project="MyoHandAirplaneFly-v0",
+    config=config,
+    name="brax_ppo_v0",
 )
+
+# Use the config dictionary in functools.partial
+train_fn = functools.partial(ppo.train, **config)
 
 
 make_inference_fn, params, _ = train_fn(environment=env, progress_fn=progress)
 
 print(f"time to jit: {times[1] - times[0]}")
 print(f"time to train: {times[-1] - times[1]}")
+
+if not os.path.exists("policies"):
+    os.makedirs("policies", exist_ok=True)
+
+model.save_params('policies/myohand_airplane_v0_brax_ppo', params)
